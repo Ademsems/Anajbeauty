3705ce44fe4e41b3842fc19ba6beb1844b870e8f
Ademsems <140335883+Ademsems@users.noreply.github.com>
2026-03-27 14:35:12 +0100

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}.json`)).default,
-}));
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale;
+  return {
+    locale, // Return the locale here
+    messages: (await import(`./messages/${locale}.json`)).default
+  };
+});
