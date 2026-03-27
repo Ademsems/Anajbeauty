9edf438f453d59a8de55a070920999e70918f1d7
Ademsems <140335883+Ademsems@users.noreply.github.com>
2026-03-27 14:29:51 +0100

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import type { Metadata } from 'next';
+export const dynamic = 'force-dynamic';
 
 const locales = ['fr', 'en'] as const;
 type Locale = (typeof locales)[number];
